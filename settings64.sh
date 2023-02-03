8904e694370148aaffe817ad1a5aa31b14a0d0d7
Mason Haynes <77853043+haynesm1@users.noreply.github.com>
2023-02-03 10:49:13 -0500

    Add files via upload

---
+++ settings64.sh
@@ -0,0 +1,8 @@
+##############################################################
+# Copyright (c) 1986-2023 Xilinx, Inc.  All rights reserved. #
+##############################################################
+
+source '/mnt/c/Xilinx/Vivado/2020.3/.settings64-Vivado.sh'
+source '/mnt/c/Xilinx/DocNav/.settings64-DocNav.sh'
+source '/mnt/c/Xilinx/Model_Composer/2020.3/.settings64-Model_Composer.sh'
+source '/mnt/c/Xilinx/Vitis_HLS/2020.3/.settings64-Vitis_HLS.sh'
